e86124daff00a98d54c36de207e313adc6d35a45
Stephen R. van den Berg <srb@cuci.nl>
2020-12-16 09:43:20 +0100

    Add benchmarks.

--- runtest.sh
+++ runtest.sh
@@ -3,12 +3,14 @@
 dirs="$@"
 test -z "$dirs" && dirs="*"
 
+iters=1000
+
 ok=0
 
 for dir in testsuite/$dirs
 do
   rm -f $dir/output.*
-  node - <<HERE
+  result="$(node - <<HERE
 const fs = require("fs");
 var rxml = require("./remixml.min.js");
 //var rxml = require("./remixml.js");
@@ -20,15 +22,25 @@ var jssrc = rxml.remixml2js(remixmlsrc);
 fs.writeFileSync("$dir/output.min.js", jssrc);
 var macrofn = rxml.js2obj(jssrc);
 
-var abstract = macrofn({"_":eval("("+data+")")});
+data = {"_":eval("("+data+")")};
+
+var abstract;
+var iters = $iters;
+var t1 = new Date();
+while (iters--)
+  abstract = macrofn(data);
+var t2 = new Date();
+var dt = (t2-t1)/$iters;
+console.log(dt);
 fs.writeFileSync("$dir/output.remixml", rxml.abstract2txt(abstract));
 HERE
+  )"
   js-beautify -s 2 $dir/output.min.js >$dir/output.js
   if cmp -s $dir/target.remixml $dir/output.remixml
   then
-    echo Succeed: $dir
+    echo "Succeed: $dir	  $result"
   else
-    echo "************************** Failed: $dir"
+    echo "****************************************** Failed: $dir"
     ok=1
     ( cat <<\HERE
 <html>
